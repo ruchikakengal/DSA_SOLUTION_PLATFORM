c7f5fc03d745217cf41048168726013537789f7a
Gautamchandar <gautamnogai@gmail.com>
2025-01-09 15:27:45 +0530

    Updated
    I changed footer Ui as well as responsiveness and add animation using gsap

---
+++ script.js
@@ -0,0 +1,13 @@
+ var tl = gsap.timeline()
+ tl.from("h1", {
+    y:-20,
+    opacity:0,
+    delay:0.5,
+    duration:1
+ })
+ tl.from(".content-wrapper p", {
+    y:-20,
+    opacity:0,
+    delay:0.5,
+    duration:1
+ })

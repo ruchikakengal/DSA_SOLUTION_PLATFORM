1dc12eb5b209b30ae35dedba5026a1f46a709f49
MrunalxMehar <mrunalmehar2020@gmail.com>
2025-01-11 14:06:22 +0530

    added code-line-num and code-editor

--- pages/leetcode.js
+++ pages/leetcode.js
@@ -3,7 +3,6 @@ function handleViewSolution(problemTitle) {
     
     let solutionText = "";
 
-    // Solutions for each problem
     if (problemTitle === "Two Sum") {
         solutionText = `
             <div class="card">
@@ -12,20 +11,7 @@ function handleViewSolution(problemTitle) {
                 </div>
                 <div class="card-content">
                     <p>Given an array of integers, return the indices of the two numbers such that they add up to a specific target. You may assume that each input would have exactly one solution, and you may not use the same element twice.</p>
-                    <pre>
-                    <code class="language-javascript">
-function twoSum(nums, target) {
-    let map = new Map();
-    for (let i = 0; i < nums.length; i++) {
-        let complement = target - nums[i];
-        if (map.has(complement)) {
-            return [map.get(complement), i];
-        }
-        map.set(nums[i], i);
-    }
-}
-                    </code>
-                    </pre>
+                    <div id="ace-editor-two-sum" style="height: 200px;"></div>
                     <p>Time Complexity: O(n)</p>
                     <p>Space Complexity: O(n)</p>
                 </div>
@@ -39,21 +25,7 @@ function twoSum(nums, target) {
                 </div>
                 <div class="card-content">
                     <p>Given the head of a singly linked list, reverse the list and return its head.</p>
-                    <pre>
-                    <code class="language-javascript">
-function reverseList(head) {
-    let prev = null;
-    let curr = head;
-    while (curr !== null) {
-        let nextTemp = curr.next;
-        curr.next = prev;
-        prev = curr;
-        curr = nextTemp;
-    }
-    return prev;
-}
-                    </code>
-                    </pre>
+                    <div id="ace-editor-reverse-list" style="height: 200px;"></div>
                     <p>Time Complexity: O(n)</p>
                     <p>Space Complexity: O(1)</p>
                 </div>
@@ -67,27 +39,7 @@ function reverseList(head) {
                 </div>
                 <div class="card-content">
                     <p>Given the root of a binary tree, return the level order traversal of its nodes' values.</p>
-                    <pre>
-                    <code class="language-javascript">
-function levelOrder(root) {
-    if (!root) return [];
-    let result = [];
-    let queue = [root];
-    while (queue.length > 0) {
-        let levelSize = queue.length;
-        let level = [];
-        for (let i = 0; i < levelSize; i++) {
-            let node = queue.shift();
-            level.push(node.val);
-            if (node.left) queue.push(node.left);
-            if (node.right) queue.push(node.right);
-        }
-        result.push(level);
-    }
-    return result;
-}
-                    </code>
-                    </pre>
+                    <div id="ace-editor-level-order" style="height: 200px;"></div>
                     <p>Time Complexity: O(n)</p>
                     <p>Space Complexity: O(n)</p>
                 </div>
@@ -108,5 +60,56 @@ function levelOrder(root) {
 
     solutionContent.innerHTML = solutionText;
 
-    Prism.highlightAll();
+    
+    if (problemTitle === "Two Sum") {
+        var editor = ace.edit("ace-editor-two-sum");
+        editor.setTheme("ace/theme/monokai");
+        editor.session.setMode("ace/mode/javascript");
+        editor.setValue(`function twoSum(nums, target) {
+    let map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+        if (map.has(complement)) {
+            return [map.get(complement), i];
+        }
+        map.set(nums[i], i);
+    }
+}`);
+    } else if (problemTitle === "Reverse Linked List") {
+        var editor = ace.edit("ace-editor-reverse-list");
+        editor.setTheme("ace/theme/monokai");
+        editor.session.setMode("ace/mode/javascript");
+        editor.setValue(`function reverseList(head) {
+    let prev = null;
+    let curr = head;
+    while (curr !== null) {
+        let nextTemp = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = nextTemp;
+    }
+    return prev;
+}`);
+    } else if (problemTitle === "Binary Tree Level Order Traversal") {
+        var editor = ace.edit("ace-editor-level-order");
+        editor.setTheme("ace/theme/monokai");
+        editor.session.setMode("ace/mode/javascript");
+        editor.setValue(`function levelOrder(root) {
+    if (!root) return [];
+    let result = [];
+    let queue = [root];
+    while (queue.length > 0) {
+        let levelSize = queue.length;
+        let level = [];
+        for (let i = 0; i < levelSize; i++) {
+            let node = queue.shift();
+            level.push(node.val);
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+        }
+        result.push(level);
+    }
+    return result;
+}`);
+    }
 }
